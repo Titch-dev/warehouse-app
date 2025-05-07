4c937eba31ae8a2d3488985bfd70dab90a9b396d
Titch-dev <lewis.d.perry@gmail.com>
2025-05-07 22:19:31 +0200

    events page built, ammendments to home

--- components/events/event_item.js
+++ components/events/event_item.js
@@ -1,24 +1,46 @@
+import Image from 'next/image';
+
 import styles from './event_item.module.css';
+import EventIcons from './event_icons';
+import { rubikFont } from '@/lib/fonts';
 
 
-const event_info = {
-    title: "karaoke",
-    description: `Get ready to hit the stage and belt out your favorite 
-                tunes at our Karaoke Night! Whether you're a shower singer 
-                or a rockstar in the making, this is your chance to shine. 
-                Grab a mic, enjoy awesome drinks, and vibe with a crowd that 
-                loves a good time. No judgment—just pure fun!`,
-    date: "Thursday 03 April",
-    timeStart: "7pm",
-    timeEnd: "11pm",
-    price: "R 10"
-}
+import TornBackground from '@/public/assets/patterns/torn_background.svg';
 
-export default function EventItem() {
+
+export default function EventItem({ props, isReversed }) {
+    const { id, title, description, start, end, price, img, alt} = props;
+
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    const eventDate = new Intl.DateTimeFormat("en-GB", {
+        weekday: "short",
+        day: "2-digit",
+        month: "short",
+        timeZone: "UTC",
+    }).format(startDate);
+    
+    const options = {
+        hour: "numeric",
+        hour12: true,
+        timeZone: "UTC"
+    }
+
+    const startTime = startDate.toLocaleTimeString("en-GB", options).toLowerCase();
+    const endTime = endDate.toLocaleTimeString("en-GB", options).toLowerCase();
+
+    const eventMeta = { date: eventDate, price: price, start: startTime, end: endTime};
+
   return (
-    <div className={styles.container}>
-        <h1 className={styles.date}>{event_info['date']}</h1>
-
+    <div className={`${styles.container} ${isReversed ? styles.reversed : ""}`}>
+        <TornBackground className={styles.event_bg}/>
+        <div className={styles.content}>
+          <h1 className={`${rubikFont.className} ${styles.event_title}`}>{title}</h1>
+          <Image src={img} className={styles.image} width={300} height={450} alt={alt}></Image>
+          <EventIcons {...eventMeta} />
+          <p className={styles.description}>{description}</p>
+        </div>
     </div>
   )
 }
