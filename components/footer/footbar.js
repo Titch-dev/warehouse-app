91578150da607f761f77c259bdb4ad3486bd994e
Titch-dev <lewis.d.perry@gmail.com>
2025-05-16 19:39:34 +0200

    footer component built for desktop

--- components/footer/footbar.js
+++ components/footer/footbar.js
@@ -1,40 +1,73 @@
-import Link from 'next/link';
+'use client'
 
-import StaticMap from './static_map';
+import Link from 'next/link';
+import { useState } from 'react';
+
+import Modal from '../modal';
+import Contact from '../contact/contact';
 
 import styles from './footbar.module.css';
+import { colors } from '@/lib/colors';
+
+import WHLogo from '@/components/assets/whLogoSVG';
+import MapIconSVG from '../assets/icons/mapIconSVG';
+import EnvelopeIcon from '../assets/icons/envelope_IconSVG';
+import TornBorder from '../assets/patterns/torn_border';
+import Socials from '../assets/icons/socials';
 
 export default function Footbar() {
-  const fBUrl = "https://www.facebook.com/TheWestvilleWarehouse/";
-  const instaUrl = "https://www.instagram.com/thewestvillewarehouse/";
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const mapUrl = `https://www.google.com/maps/search/?api=1&query=The+Westville+Warehouse`
+
 
   return (
-    <div className={styles.container}>
-      <StaticMap></StaticMap>
-      <div className={styles.contact_content}>
-        <div className={styles.address}>
-          <p>48a Buckingham Terrace,</p>
-          <p>Westville,</p>
-          <p>Durban,</p>
-          <p>4001,</p>
-          <p>South Africa</p>
+    <div className={styles.wrapper}>
+      <TornBorder top={true} color={colors.greydark4} />
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <WHLogo className={styles.logo} />
+          <div className={styles.nav_container}>
+            <h2>Navigation</h2>
+            <Link href="/">Home</Link>
+            <Link href="/events">Events</Link>
+            <Link href="/menu">Menus</Link>
+            <Link href="/gallery">Gallery</Link>
+            <Link href="/login">Login</Link>
+          </div>
+          <div className={styles.list}>
+            <button 
+              className={styles.item_row}
+              onClick={() => setIsModalOpen(true)}>
+              <EnvelopeIcon>
+                <linearGradient id="Gradient1" x2="0" y2="1">
+                    <stop className={styles.stop1} offset="0%" />
+                    <stop className={styles.stop2} offset="50%" />
+                    <stop className={styles.stop3} offset="100%" />
+                </linearGradient>
+              </EnvelopeIcon>
+              <p style={{alignSelf: 'center'}}>Contact us...</p>
+            </button>
+            <Link className={styles.item_row} href={mapUrl} target='_blank'>
+              <MapIconSVG >
+                <linearGradient id="Gradient1" x2="0" y2="1">
+                    <stop className={styles.stop1} offset="0%" />
+                    <stop className={styles.stop2} offset="50%" />
+                    <stop className={styles.stop3} offset="100%" />
+                </linearGradient>
+              </MapIconSVG>
+              <p>48a Buckingham Terrace,<br/>Westville,<br/>Durban,<br/>
+              4001,<br/>South Africa</p>
+            </Link>
+          </div>
         </div>
-        <div className={styles.socials_container}>
-          <Link href={fBUrl} target='_blank'>
-            <svg className={styles.socials_icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/>
-            </svg>
-          </Link>
-          <Link href={instaUrl} target="_blank">
-            <svg className={styles.socials_icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
-            </svg>
-          </Link>
+        <div className={`${styles.content} ${styles.content_border}`}>
+          <p className={styles.copy_right}>&copy; 2025 The Westville Warehouse</p>
+          <Socials size={30}/>
         </div>
-        <Link href="/contact" className={styles.contact_link}>
-          <h1>... Contact Us</h1>
-        </Link>
       </div>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} >
+        <Contact/>
+      </Modal>
     </div>
   );
 }
